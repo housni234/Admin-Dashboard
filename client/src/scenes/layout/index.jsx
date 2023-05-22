ba68b5e7e108d604ef6277ee61f5b4e504fbe43d
Housni <alami.housni@gmail.com>
2023-05-22 13:26:53 +0200

    Navbar layout completed

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar"
 
 const Layout = () => {
-    return <div>layout</div>
+    return <Box width="100%" height="100%">
+        <Box>
+            <Navbar />
+            <Outlet />
+        </Box>
+    </Box>
 }
 
 export default Layout
