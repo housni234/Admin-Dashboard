c655c20badb140d957c6ece6c79c40892ebaee19
Housni <alami.housni@gmail.com>
2023-06-09 13:42:12 +0200

    connecting backend with frontend in progress

---
+++ server/controllers/general.js
@@ -0,0 +1,11 @@
+import user from "../models/User.js";
+
+export const getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// import data
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -33,4 +37,5 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+    User.insertMany(dataUser)
 }).catch((error) => console.log(`${error} did not connect`))
